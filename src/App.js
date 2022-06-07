00f4da2bfca0d312c1bd97be00a9b00e0cebf990
Neil <neil.bennett01@gmail.com>
2022-06-07 15:54:49 -0400

    local storage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import "antd/dist/antd.css";
 
 function App() {
+  const [token, setToken] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!token ? <Login setToken={setToken} /> : <Home token={token} />}
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,53 @@
+import { Button, Form, Input } from "antd";
+import bcrypt from "bcryptjs";
+
+const salt = "$2b$10$3c22hEeSjfAnsuZ76BHS0O";
+
+export default function Login({ setToken }) {
+  const handleLogin = ({ email, password }) => {
+    console.log(email, password);
+    const hash = bcrypt.hashSync(password, salt);
+    console.log(hash);
+    fetch("http://localhost:5050/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password: hash }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          alert(data.error);
+          return;
+        }
+        console.log(data.token);
+        localStorage.setItem("token", data.token); //allows token to be stored in localStorage
+        setToken(data.token);
+      })
+      .catch((error) => console.log(error));
+  };
+  return (
+    <>
+      <h1>Login</h1>
+      <Form
+        name="login"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        onFinish={handleLogin}
+      >
+        <Form.Item name="email" label="Email">
+          <Input />
+        </Form.Item>
+        <Form.Item name="password" label="Password">
+          <Input.Password />
+        </Form.Item>
+        <Form.Item wrapperCol={{ span: 16, offset: 8 }}>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+}
