26c23ec5cab515302b12c61fb5417bb4d0651cc2
Neil <neil.bennett01@gmail.com>
2022-06-07 16:27:05 -0400

    send token to private api point to get message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import "antd/dist/antd.css";
 
 function App() {
   const [token, setToken] = useState("");
+  useEffect(() => {
+    const _token = localStorage.getItem("token");
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
   return (
     <div className="App">
       {!token ? <Login setToken={setToken} /> : <Home token={token} />}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,35 @@
-export default function Home() {
+import { useState } from "react";
+import { Alert, Button } from "antd";
+
+export default function Home({ token }) {
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+  const getPrivateStuff = () => {
+    fetch("http://localhost:5050/private", {
+      headers: {
+        Authorization: token,
+      },
+    })
+      .then((res) => res.json)
+      .then((data) => {
+        if (data.error) {
+          setError(data.error);
+          setMessage("");
+          return;
+        }
+        setMessage(data.message);
+        setError("");
+      })
+      .catch((err) => console.error(err));
+  };
   return (
     <>
       <h1>Home</h1>
+      {message && <Alert message={message} type="success" />}
+      {error && <Alert message={error} type="error" />}
+      <Button onClick={getPrivateStuff} type="primary" size="large">
+        Get Private Stuff
+      </Button>
     </>
   );
 }
